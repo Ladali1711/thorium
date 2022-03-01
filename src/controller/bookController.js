e1ed6ed86e1a4ceb98cafb7ac63effe7165c0060
our_own_username <our_own_email_id>
2022-03-01 20:53:02 +0530

    mongooes assignment

---
+++ src/controller/bookController.js
@@ -0,0 +1,64 @@
+// const { reset } = require('nodemon')
+const BookModel = require('../models/bookmodel')
+
+
+const createBook = async function(req, res){
+let bookData = req.body
+
+let book = await BookModel.create(bookData)
+res.send({msg: book})
+
+}
+
+const getBook = async function (req, res){
+
+    let book = await BookModel.find().select({bookName: 1, authorName: 1, _id : 0})
+    res.send({msg: book})
+}
+
+const booksInYear = async function (req, res){
+
+    let year = req.body.year
+
+    let book = await BookModel.find({year : year})
+    res.send({msg: book})
+}
+
+const particularBooks = async function (req, res){
+
+    let arr = Object.keys(req.body) 
+    let property = arr[0] 
+    let value = req.body[property] 
+    
+     console.log(property)
+    console.log(value)
+    
+
+    let book = await BookModel.find({property : value})
+    res.send({msg: book})
+}
+
+const getINRBooks = async function(req,res){
+
+        
+    let book = await BookModel.find({"price.indianPrice" :  {$in : ["100 RS", "200 RS", "500 RS"]}}) 
+    res.send({msg: book})
+}
+
+const getRandomBooks = async function(req,res){
+    let book = await BookModel.find({$or: [{totalPages : {$gt : 500}}, {stockAvailable: {$eq : true}}]})
+    res.send({msg : book})
+}
+
+
+
+
+
+
+
+module.exports.createBook = createBook
+module.exports.getBook = getBook
+module.exports.booksInYear = booksInYear
+module.exports.particularBooks = particularBooks
+module.exports.getINRBooks = getINRBooks
+module.exports.getRandomBooks = getRandomBooks
