ae615cf59a6bdae03c67e0a5560736558569df90
our_own_username <our_own_email_id>
2022-03-02 23:33:30 +0530

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+// const mongoose=require('mongoose')
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-
+// const DB = ' mongodb+srv://ladalipravin:Ktxildd7HtOatr5Y@cluster0.mt4xp.mongodb.net/ladali?retryWrites=true&w=majority'
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,8 +12,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
+mongoose.connect( "mongodb+srv://ladalipravin:Ktxildd7HtOatr5Y@cluster0.mt4xp.mongodb.net/ladali?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifideTopology:true,
+    useFindAndModify:false
+
+
+})
 .then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+.catch ( err => console.log('no connection') )
 
 app.use('/', route);
 
@@ -20,3 +29,6 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+// https://mongoosejs.com/docs/populate.html
+
